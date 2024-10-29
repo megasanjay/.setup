029764816eb36a9a15ef41316f2ba4af8f01aa62
Sanjay Soundarajan <sanjay.16@live.com>
2024-10-29 10:46:19 -0700

    feat: add mouse pointer finding

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -6,3 +6,6 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Ner
 
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# Show the mouse cursor when ctrl is hit
+gsettings set org.gnome.desktop.interface locate-pointer true
